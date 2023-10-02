cd5744b5889244628563393bad863344f3cf694f
daorozc <daorozc@bancolombia.com.co>
2023-10-02 07:31:32 -0500

    feat(launch.json): add launch configuration for Python files to enable debugging in VS Code feat(skyscanner.py): improve user experience by allowing selection of national or international airports and input validation for airport codes

--- skyscanner.py
+++ skyscanner.py
@@ -1,75 +1,127 @@
 """
-File to open all the URLs of the flights from Medellin to the cities
+Help with the planning of a trip
 """
-
-import webbrowser
 
 base_url = "https://www.espanol.skyscanner.com/transporte/vuelos"
 initial_date = "240506"
 final_date = "240514"
-airports = {
-    #  "Jose Maria Cordova International Airport - Colombia"
-    "source": "MDE",
-    "targets": [
-        # "Ciudad de Panama International Airport - Panama"
-        "PTY",
-        # "Cancun International Airport - Mexico"
-        "CUN",
-        # "Ciudad de Mexico International Airport - Mexico"
-        "MEX",
-        # "Punta Cana International Airport - Dominican Republic"
-        "PUJ",
-        # "Santo Domingo Airport - Dominican Republic"
-        "SDQ",
-        # "Jose Marti International Airport - Cuba"
-        "HAV",
-        # "Queen Beatrix International Airport - Aruba"
-        "AUA",
-        # "Hato International Airport - Curacao"
-        "CUR",
-        # "Juan Santamaria International Airport - Costa Rica"
-        "SJO",
-        # "Quito International Airport - Ecuador"
-        "UIO",
-        # "Miami International Airport - United States"
-        "MIA",
-        # "Orlando International Airport - United States"
-        "MCO",
-        # "Lima International Airport - Peru"
-        "LIM",
-        # "Cusco International Airport - Peru"
-        "CUZ",
-        # "Asuncion International Airport - Paraguay"
-        "ASU",
-        # "Montevideo International Airport - Uruguay"
-        "MVD",
-        # "Santiago de Chile International Airport - Chile"
-        "SCL",
-        # "La paz International Airport - Bolivia"
-        "LPB",
-        # "Rio de Janeiro International Airport - Brazil"
-        "GIG",
-        # "Buenos Aires International Airport - Argentina"
-        "EZE",
-        # "Madrid International Airport - Spain"
-        "MAD",
-        # "Londres International Airport - England"
-        "LON",
-    ],
-}
 
+airport_national_codes = [
+    "MDE: Jose Maria Cordova International Airport - Medellin - Colombia",
+    "BOG: El Dorado International Airport - Bogota - Colombia",
+    "CTG: Rafael Nunez International Airport - Cartagena - Colombia",
+    "PEI: Matecana International Airport - Pereira - Colombia",
+    "AXM: El Eden International Airport - Armenia - Colombia",
+    "CLO: Alfonso Bonilla Aragon International Airport - Cali - Colombia",
+    "BAQ: Ernesto Cortissoz International Airport - Barranquilla - Colombia",
+    "ADZ: Gustavo Rojas Pinilla International Airport - San Andres - Colombia",
+    "LET: Alfredo Vasquez Cobo International Airport - Leticia - Colombia",
+    "BGA: Palonegro International Airport - Bucaramanga - Colombia",
+    "CUC: Camilo Daza International Airport - Cucuta - Colombia",
+    "EYP: El Yopal Airport - Yopal - Colombia",
+    "MTR: Los Garzones Airport - Monteria - Colombia",
+    "MZL: La Nubia Airport - Manizales - Colombia",
+    "NVA: Benito Salas Airport - Neiva - Colombia",
+    "PPN: Guillermo Leon Valencia Airport - Popayan - Colombia",
+    "PSO: Antonio Narino Airport - Pasto - Colombia",
+    "RCH: Almirante Padilla Airport - Riohacha - Colombia",
+    "SMR: Simon Bolivar International Airport - Santa Marta - Colombia",
+    "VUP: Alfonso Lopez Pumarejo Airport - Valledupar - Colombia",
+    "VVC: Vanguardia Airport - Villavicencio - Colombia",
+]
+
+airport_international_codes = [
+    "MDE: Jose Maria Cordova International Airport - Medellin - Colombia",
+    "PTY: Ciudad de Panama International Airport - Panama",
+    "CUN: Cancun International Airport - Mexico",
+    "MEX: Ciudad de Mexico International Airport - Mexico",
+    "PUJ: Punta Cana International Airport - Dominican Republic",
+    "SDQ: Santo Domingo Airport - Dominican Republic",
+    "HAV: Jose Marti International Airport - Cuba",
+    "AUA: Queen Beatrix International Airport - Aruba",
+    "CUR: Hato International Airport - Curacao",
+    "SJO: Juan Santamaria International Airport - Costa Rica",
+    "UIO: Quito International Airport - Ecuador",
+    "MIA: Miami International Airport - United States",
+    "MCO: Orlando International Airport - United States",
+    "LIM: Lima International Airport - Peru",
+    "CUZ: Cusco International Airport - Peru",
+    "ASU: Asuncion International Airport - Paraguay",
+    "MVD: Montevideo International Airport - Uruguay",
+    "SCL: Santiago de Chile International Airport - Chile",
+    "LPB: La paz International Airport - Bolivia",
+    "GIG: Rio de Janeiro International Airport - Brazil",
+    "EZE: Buenos Aires International Airport - Argentina",
+    "MAD: Madrid International Airport - Spain",
+    "LON: Londres International Airport - England",
+]
+
+airport_mode = ""
+airports = []
+
+while airport_mode != "nac" and airport_mode != "nonac":
+    airport_mode = input("\nIngresar modo de aeropuerto (nac/nonac): ")
+    if airport_mode == "nac":
+        print("\n>>>Aeropuertos nacionales")
+        airports = airport_national_codes
+    elif airport_mode == "nonac":
+        print("\n>>>Aeropuertos internacionales")
+        airports = airport_international_codes
+    else:
+        print("!!!Modo de aeropuerto no valido!!!")
+
+airport_codes = [airport.split(":")[0] for airport in airports]
+
+# Print all airports
+for airport in airports:
+    print(airport)
+
+source_airport = ""
+target_airports = []
+
+# Loop for source airport
+while True:
+    # Ask user for source airport
+    source_airport = input("\nIngresar codigo aeropuerto origen: ")
+    source_airport = source_airport.upper()
+
+    # Check if source airport is valid
+    if source_airport in airport_codes:
+        break
+    else:
+        print("!!!Airport code not valid!!!")
+
+# Loop for target airports
+while True:
+    # Ask user for target airports
+    target_airports = input(
+        "\nIngresar codigo aeropuerto destino (separados por coma): "
+    ).split(",")
+    target_airports = [airport.strip().upper() for airport in target_airports]
+
+    # Check if target airports are valid
+    if all(airport.strip() in airport_codes for airport in target_airports):
+        break
+    else:
+        print("!!!Airport codes not valids!!!")
 
 # Open each URL in a new tab
-for target in airports["targets"]:
-    url = f"{base_url}/{airports['source']}/{target}/{initial_date}/{final_date}"
+for target_airport in target_airports:
+    print("\n##################################################################")
+    print(f"Desde {source_airport} hasta {target_airport}")
 
-    webbrowser.open_new_tab(url)
+    print("\n>>>Vuelos")
+    url = f"{base_url}/{source_airport}/{target_airport}/{initial_date}/{final_date}"
+    print(url)
 
+    print("\n>>>Hotel")
+
+    print("\n>>>Itinerario")
     print(
         f"\nCrear un itinerario detallado del viaje teniendo en cuenta la siguiente información:"
         f"\n-Dos personas"
-        f"\n-Aeropuerto de origen: {airports['source']}"
-        f"\n-Aeropuerto de destino: {target}"
+        f"\n-Aeropuerto de origen: {source_airport}"
+        f"\n-Aeropuerto de destino: {target_airport}"
         f"\n-Fecha de ida: {initial_date}"
         f"\n-Fecha de regreso: {final_date}"
         "\n-Incluir horas de cada actividad"
